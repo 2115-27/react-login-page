24b26dcf4a3c48c3c63988447b85a8d4a2469a96
shiyamsundark <105621600+shiyamsundark@users.noreply.github.com>
2023-01-02 12:00:28 +0530

    Update HandleLogin.jsx

--- src/components/HandleLogin.jsx
+++ src/components/HandleLogin.jsx
@@ -1,9 +1,37 @@
 import React from "react";
+// import { Octokit } from "octokit";
 
+// const handleClick = async () => {
+// 	  console.log("handleclick")
+// 	  const octokit = new Octokit({
+//   		auth: process.env.TOKEN,
+// 	})
+    
+// await octokit.request('POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches', {
+//   owner: 'Malavika1113',
+//   repo: 'react-workflow-gh-actions',
+//   workflow_id: 'Reuseable-A.yml',
+//   ref: 'master'
+    
 const Home = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Data Ext Form
+        </p>
+        <form>
+         <div className="input-container">
+           <label>Release Version </label>
+           <input type="text" name="uname" required />
+         </div>
+         <div className="input-container">
+           <label>Model Type </label>
+           <input type="text" name="pass" required />
+         </div>
+         <button onClick={handleClick}>BUILD</button>
+        </form>
+      </header>
     </div>
   );
 };
